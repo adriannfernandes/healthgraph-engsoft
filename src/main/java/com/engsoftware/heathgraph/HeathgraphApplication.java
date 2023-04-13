41d241bd71f8f866d8d9494228f501ce14b2feb5
Adrian <adrianancelmo1098@gmail.com>
2023-04-13 14:18:53 -0300

    First commit

---
+++ src/main/java/com/engsoftware/heathgraph/HeathgraphApplication.java
@@ -0,0 +1,13 @@
+package com.engsoftware.heathgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeathgraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeathgraphApplication.class, args);
+	}
+
+}
