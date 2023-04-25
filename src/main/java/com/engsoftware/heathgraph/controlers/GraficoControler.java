291e9a09778d6a22d63d0b3e5042ebfed0c8ec77
nathan <nathanjfa@outlook.com>
2023-04-25 02:37:17 -0300

    Graficos sendo gerados

--- src/main/java/com/engsoftware/heathgraph/controlers/GraficoControler.java
+++ src/main/java/com/engsoftware/heathgraph/controlers/GraficoControler.java
@@ -25,14 +25,18 @@ public class GraficoControler {
     @Autowired
     private final RegistroService registroService;
 
-    @GetMapping("/pesoXaltura")
-    public String retornaTeste() {
-        return "pesoXaltura";
-    }
+    @GetMapping("/pesoXidade")
+    public String retornaGraficoPesoXidade() { return "pesoXidade"; }
+    @GetMapping("/imc")
+    public String retornaGraficoImc() {        return "imc";    }
+    @GetMapping("/alturaXidade")
+    public String retornaGraficoAlturaXidade() { return "alturaXidade"; }
+    @GetMapping("/imcXidade")
+    public String retornaGraficoImcXidade() {   return "imcXidade";  }
 
-    @GetMapping("/gerarGraficoPesoXaltura")
+    @GetMapping("/gerarGraficoPesoXidade")
     @ResponseBody
-    public byte[] gerarGraficoPesoXaltura() throws IOException {
+    public byte[] gerarGraficoPesoXidade() throws IOException {
 
         // Dados de exemplo (médias de peso por idade para meninos)
         int[] idades = {0, 1, 2, 3, 4, 5};
@@ -49,7 +53,91 @@ public class GraficoControler {
                 "Idade (anos)", "Peso (kg)", dataset, PlotOrientation.VERTICAL, true, true, false);
 
         // Criação da imagem
-        BufferedImage bufferedImage = chart.createBufferedImage(500, 300);
+        BufferedImage bufferedImage = chart.createBufferedImage(1400, 1000);
+
+        // Conversão para bytes
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "png", baos);
+
+        return baos.toByteArray();
+    }
+
+    @GetMapping("/gerarGraficoAlturaXidade")
+    @ResponseBody
+    public byte[] gerarGraficoAlturaXidade() throws IOException {
+
+        // Dados de exemplo (médias de altura por idade para meninos)
+        double[] alturas = {0, 1, 2, 3, 4, 5};
+        int[] idade = {3, 9, 12, 15, 17, 90};
+
+        // Criação do dataset
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        for (int i = 0; i < alturas.length; i++) {
+            dataset.addValue(idade[i], "Altura média", Double.toString(alturas[i]));
+        }
+
+        // Criação do gráfico
+        JFreeChart chart = ChartFactory.createLineChart("Curva de crescimento de altura por idade (meninos)",
+                "Altura (cm)", "Idade (anos)", dataset, PlotOrientation.VERTICAL, true, true, false);
+
+        // Criação da imagem
+        BufferedImage bufferedImage = chart.createBufferedImage(1400, 1000);
+
+        // Conversão para bytes
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "png", baos);
+
+        return baos.toByteArray();
+    }
+
+    @GetMapping("/gerarGraficoImcXidade")
+    @ResponseBody
+    public byte[] gerarGraficoImcXidade() throws IOException {
+
+        // Dados de exemplo (médias de peso por idade para meninos)
+        int[] idades = {0, 1, 2, 3, 4, 5};
+        double[] imc = {3.4, 9.5, 12.6, 15.3, 17.2, 90.0};
+
+        // Criação do dataset
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        for (int i = 0; i < idades.length; i++) {
+            dataset.addValue(imc[i], "IMC médio", Integer.toString(idades[i]));
+        }
+
+        // Criação do gráfico
+        JFreeChart chart = ChartFactory.createLineChart("Curva de crescimento do IMC por idade (meninos)",
+                "Idade (anos)", "IMC", dataset, PlotOrientation.VERTICAL, true, true, false);
+
+        // Criação da imagem
+        BufferedImage bufferedImage = chart.createBufferedImage(1400, 1000);
+
+        // Conversão para bytes
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "png", baos);
+
+        return baos.toByteArray();
+    }
+
+    @GetMapping("/gerarGraficoImc")
+    @ResponseBody
+    public byte[] gerarGraficoImc() throws IOException {
+
+        // Dados de exemplo (médias de peso por idade para meninos)
+        double[] alturas = {1.60, 1.61, 1.62, 1.63, 1.64, 1.65};
+        double[] pesos = {3.4, 9.5, 12.6, 15.3, 17.2, 90.0};
+
+        // Criação do dataset
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        for (int i = 0; i < alturas.length; i++) {
+            dataset.addValue(pesos[i], "Peso médio", Double.toString(alturas[i]));
+        }
+
+        // Criação do gráfico
+        JFreeChart chart = ChartFactory.createLineChart("Curva de crescimento do IMC (meninos)",
+                "Altura (cm)", "Peso (kg)", dataset, PlotOrientation.VERTICAL, true, true, false);
+
+        // Criação da imagem
+        BufferedImage bufferedImage = chart.createBufferedImage(1400, 1000);
 
         // Conversão para bytes
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
--- src/main/java/com/engsoftware/heathgraph/repository/RegistroRepository.java
+++ src/main/java/com/engsoftware/heathgraph/repository/RegistroRepository.java
@@ -3,8 +3,9 @@ package com.engsoftware.heathgraph.repository;
 import java.util.List;
 import com.engsoftware.heathgraph.entities.Registro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RegistroRepository extends JpaRepository<Registro, Long> {
-    
+//    @Query("SELECT * FROM crianca WHERE idCrianca = :idCrianca")
 //    List<Registro> findAllByIdCrianca (Long idCrianca);
 }
