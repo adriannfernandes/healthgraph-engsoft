825c2d21ff6d0348c8c46c5ddb651584981a56b8
TioSam02 <w02.barcelos@gmail.com>
2023-04-24 23:10:53 -0300

    findById

--- src/main/java/com/engsoftware/heathgraph/service/CriancaService.java
+++ src/main/java/com/engsoftware/heathgraph/service/CriancaService.java
@@ -1,7 +1,23 @@
 package com.engsoftware.heathgraph.service;
 
+import com.engsoftware.heathgraph.entities.Crianca;
+import com.engsoftware.heathgraph.repository.CriancaRepository;
+import com.engsoftware.heathgraph.service.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CriancaService {
+
+    @Autowired
+    private CriancaRepository criancaRepository;
+
+    public Crianca findById(Long id) {
+        Optional<Crianca> criancaOptional = criancaRepository.findById(id);
+
+        return criancaOptional.orElseThrow(() -> new ObjectNotFoundException(
+                "Criança não encontrada. Id: " + id + ", Tipo: " + Crianca.class.getName()));
+    }
 }
